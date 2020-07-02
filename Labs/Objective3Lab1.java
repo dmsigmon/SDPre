c46f7c761b2ba3ea7f9561a9c4c3ba0841b17952
Douglas Sigmon <douglas.sigmon@gmail.com>
2020-07-02 17:53:33 -0600

    complete Objective3Lab1.java

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,10 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+    num1 = 3;
+    num2 = 5;
+    num3 = 1;
+    average = (num1 + num2 + num3)/3;
+    System.out.println(average);
+  }
+}
