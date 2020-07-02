1813a6f0b28783d9f54aeea8f54218d26d060ab5
Douglas Sigmon <douglas.sigmon@gmail.com>
2020-07-02 17:25:23 -0600

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main (String[] args) {
+    String name = "Douglas Sigmon";
+    int age = 30;
+    String state = "North Carolina";
+
+    System.out.println("Name: " + name + " Age: " +
+    age + " State: "+ state);
+  }
+}
