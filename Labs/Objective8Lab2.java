047ac830170761803f6791517c7f347031d9dae3
Douglas Sigmon <douglas.sigmon@gmail.com>
2020-07-04 04:02:46 -0600

    complete Objective8Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,10 @@
+public class Objective8Lab2 {
+  public static void main(String[] args){
+    int sumTotal = 0;
+
+    for (int i = 0; i <= 20; i++) {
+      sumTotal += i;
+    }
+    System.out.println(sumTotal);
+  }
+}
