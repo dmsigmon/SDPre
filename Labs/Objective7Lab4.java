9dcd7f00df9730a213cb6b0ff81ce1b7836f71be
Douglas Sigmon <douglas.sigmon@gmail.com>
2020-07-04 02:45:26 -0600

    complete Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,12 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int num = 1;
+    int ans = 0;
+
+    while (num <= 20) {
+      ans += num;
+      num++;
+    }
+    System.out.println(ans);
+  }
+}
